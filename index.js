8f63b9062c3188601c94ffc7346e9556cf783af1
Itzay-HR <itzayhr@gmail.com>
2022-06-23 14:00:21 -0700

    Eliminando hoja de estilos

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ Object.keys(data).map(function (key) {
         container.innerHTML = data[key].title;
         container.style.background = data[key].background;
         container.style.color = data[key].color;
+        container.style.minHeight = "32px";
+        container.style.display = "grid";
+        container.style.alignContent = "center";
+        container.style.textAlign = "center";
+        container.style.padding = "0.5rem";
+        container.style.top = "0px";
+        container.style.left = "0px";
+        container.style.font = "normal normal bold 15px Roboto";
+
 
         const link = document.getElementById("link");
+        link.style.textDecoration = "none";
+        link.style.boxSizing = "border-box";
+
+        const body = document.getElementById("body")
+        body.style.margin = "0";
         link.setAttribute("href", `https://www.${store}.com${data[key].url}`);
     }
 })
