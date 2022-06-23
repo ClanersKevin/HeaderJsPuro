859c11e64829a91b50b58b50e7ff226f398c2e34
Itzay-HR <itzayhr@gmail.com>
2022-06-23 15:57:09 -0700

    Quitando iteraciones y usando trycatch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const headerContainerBanner = document.querySelector('#headerContainerBanner').getAttribute('data-store');
+let headerContainerBanner = document.querySelector('#headerContainerBanner').getAttribute('data-store');
 
 const a = document.createElement('a');
 a.setAttribute("id", "linkSiteBanner");
@@ -14,29 +14,30 @@ document.querySelector('#headerContainerBanner').appendChild(a);
 const response = await fetch('https://s3.us-west-2.amazonaws.com/www.claners.com/uploads/site/cintillo.json');
 const data = await response.json();
 
-Object.keys(data).map(function (key) {
-    if (key === headerContainerBanner)
-    {
-        const bannerSite = document.getElementById("bannerSite");
-        bannerSite.innerHTML = data[key].title;
-        bannerSite.style.background = data[key].background;
-        bannerSite.style.color = data[key].color;
-        bannerSite.style.minHeight = "32px";
-        bannerSite.style.display = "grid";
-        bannerSite.style.alignContent = "center";
-        bannerSite.style.textAlign = "center";
-        bannerSite.style.padding = "0.5rem";
-        bannerSite.style.top = "0px";
-        bannerSite.style.left = "0px";
-        bannerSite.style.font = "normal normal bold 15px Roboto";
+const bannerSite = document.getElementById("bannerSite");
+try 
+{
+    bannerSite.innerHTML = data[headerContainerBanner].title;
+    bannerSite.style.background = data[headerContainerBanner].background;
+    bannerSite.style.color = data[headerContainerBanner].color;
+    bannerSite.style.minHeight = "32px";
+    bannerSite.style.display = "grid";
+    bannerSite.style.alignContent = "center";
+    bannerSite.style.textAlign = "center";
+    bannerSite.style.padding = "0.5rem";
+    bannerSite.style.top = "0px";
+    bannerSite.style.left = "0px";
+    bannerSite.style.font = "normal normal bold 15px Roboto";
 
+    const linkSiteBanner = document.getElementById("linkSiteBanner");
+    linkSiteBanner.style.textDecoration = "none";
+    linkSiteBanner.style.boxSizing = "border-box";
 
-        const linkSiteBanner = document.getElementById("linkSiteBanner");
-        linkSiteBanner.style.textDecoration = "none";
-        linkSiteBanner.style.boxSizing = "border-box";
-
-        const bodyBanner = document.getElementById("bodyBanner")
-        bodyBanner.style.margin = "0";
-        linkSiteBanner.setAttribute("href", `https://www.${headerContainerBanner}.com${data[key].url}`);
-    }
-})
+    const bodyBanner = document.getElementById("bodyBanner")
+    bodyBanner.style.margin = "0";
+    linkSiteBanner.setAttribute("href", `https://www.${headerContainerBanner}.com${data[headerContainerBanner].url}`);
+} 
+catch (error) 
+{
+    console.log(error.message)
+}
