9fb211eadd74fdafe985c6b84bf8195b9f7906ef
Itzay-HR <itzayhr@gmail.com>
2022-06-23 14:52:29 -0700

    Usando document.createElement

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const store = document.querySelector('.root').getAttribute('data-store');
+const root = document.querySelector('#root').getAttribute('data-store');
+
+const a = document.createElement('a');
+a.setAttribute("id", "link");
+a.setAttribute("target", "_blank");
+a.setAttribute("target", "_blank");
+
+const div = document.createElement('div'); 
+div.setAttribute("id", "container");
+
+a.appendChild(div);
+
+document.querySelector('#root').appendChild(a);
 
 const response = await fetch('https://s3.us-west-2.amazonaws.com/www.claners.com/uploads/site/cintillo.json');
 const data = await response.json();
 
 Object.keys(data).map(function (key) {
-    if (key === store)
+    if (key === root)
     {
         const container = document.getElementById("container");
         container.innerHTML = data[key].title;
@@ -26,6 +38,6 @@ Object.keys(data).map(function (key) {
 
         const body = document.getElementById("body")
         body.style.margin = "0";
-        link.setAttribute("href", `https://www.${store}.com${data[key].url}`);
+        link.setAttribute("href", `https://www.${root}.com${data[key].url}`);
     }
 })
